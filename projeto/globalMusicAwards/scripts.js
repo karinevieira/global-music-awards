de6f77f3ed240c82acada59335a17a833ba6d276
Karine Vieira <kv.vieira1@gmail.com>
2021-06-21 15:30:07 -0300

    Update script.js

--- projeto/globalMusicAwards/scripts.js
+++ projeto/globalMusicAwards/scripts.js
@@ -1,10 +1,12 @@
 let button = document.querySelectorAll('button')
-for(let i=0; i<=5; i++){
-    button.addEventListener('click', () => {
-        button.innerHTML = 'Voted'
-        button.style.backgroundColor = 'green'
+
+for (let btn of button){
+    btn.addEventListener('click', function voted(){
+        btn.innerHTML = 'Voted'
+        btn.style.backgroundColor = 'green'
     })
 }
+
 
 
 
